8b948d15b98806b19e9c33398f4201302ba82b9e
Timon CP <timon.carneciprove@icloud.com>
2017-07-13 17:15:16 +0300

    Removed soapClient from server startup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const app = require('./app');
 const initSoapService = require('./soap/soapService');
-const callSoapClient = require('./soap/soapClient');
 
 app.listen(app.get('port'), function() {
   console.log(`\n:: Express server listening on ${app.get('port')}\n`);
   
   initSoapService(app);
-  // callSoapClient();
 });
